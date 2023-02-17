ee81b9c4ba05c66cf49e981a4d94611fa123b424
carat-johyun <johyun@carat.im>
2023-02-17 16:07:59 +0900

    initial commit

---
+++ src/main/kotlin/paradot/mobilzeo/MobilzeoApplication.kt
@@ -0,0 +1,11 @@
+package paradot.mobilzeo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MobilzeoApplication
+
+fun main(args: Array<String>) {
+	runApplication<MobilzeoApplication>(*args)
+}
---
+++ src/test/kotlin/paradot/mobilzeo/MobilzeoApplicationTests.kt
@@ -0,0 +1,13 @@
+package paradot.mobilzeo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MobilzeoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
